8846f355f4b5df68f538ec55211570f98c3443a8
msafarty <michaelsafarty@gmail.com>
2020-09-15 22:16:55 -0400

    include calendar, upload, report generation functionality

--- projMan/app.R
+++ projMan/app.R
@@ -6,7 +6,9 @@ require(shinyjs)
 require(shinyWidgets)
 require(fullcalendar)
 require(shinythemes)
-source("dataStorage/util.R")
+require(rmarkdown)
+require(knitr)
+source("projMan/dataStorage/util.R")
 
 ui <- navbarPage(
     title = "Property Management",
@@ -29,7 +31,7 @@ ui <- navbarPage(
                    shiny::selectInput(inputId = "property", label = "Property: ", choices = propertyChoices, selected = NULL)
                    ),
             column(2,
-                   actionButton(inputId = "submit", "Submit")
+                   actionButton(inputId = "submit", "Submit", width = "100%")
                    )
         ),
         fluidRow(
@@ -38,13 +40,30 @@ ui <- navbarPage(
         fluidRow(DT::dataTableOutput("taskTable"))
         ),
 tabPanel("View Calendar",
-    #test
-)
+         fluidRow(
+           column(6, 
+           actionButton(inputId = "refresh", "Sync and Load Calendar", width = '100%'),
+           offset = 3
+         )
+         ),
+         fluidRow(
+         fullcalendar::fullcalendarOutput("calendar", height = "800px") 
+         )
+  ),
+tabPanel("Upload and Download Media",
+         fluidRow(
+           helpText(HTML("<b>Only</b> accepts <b>.PNG</b> files")),
+           fileInput("upload", "Upload image", accept = "image/png"),
+           radioButtons('format', 'Document format', c('HTML', 'PDF', 'Word'),
+                        inline = TRUE),
+           downloadButton("report", "Generate Property Management Report", class = "btn-success")
+         )
+         )
 )
 
 
 server <- function(input, output) {
-
+  
    # propertyData = reactive({
    #    # properties = properties %>% filter(propertyName == input$property)
    #     req(input$taskType)
@@ -64,10 +83,11 @@ server <- function(input, output) {
       saveData(pdata, input$property)
       fullData = loadData()
       fullData = fullData[,-c(1)]
+      fullData$propertyName = as.character(fullData$propertyName)
       
       leafData = dplyr::left_join(fullData, properties, by = "propertyName")
       
-      labs <- labsForPropManagement(leafData, "propertyName", "taskType", "assignedTo" )
+      labs <- labsForMap(leafData, "propertyName", "taskType", "assignedTo" )
       
       output$map = renderLeaflet({
           leaflet(leafData) %>%
@@ -84,10 +104,85 @@ server <- function(input, output) {
       )
   })
     
+  
+  observeEvent(input$refresh, {
+  calendardata = reactiveValues()
+  calendardata = loadData()
+  
+  calendardata$color = dplyr::case_when(
+    calendardata$assignedTo == "Michael" ~ "red",
+    calendardata$assignedTo == "Nate" ~ "green",
+    calendardata$assignedTo == "Cakeo" ~ "blue"
+  )
+  calendarLabels = labsForCalendar(calendardata, "propertyName", "taskType", "assignedTo" )
+  output$calendar = renderFullcalendar({
+    caldata = data.frame(title = unlist(calendarLabels),
+                      start = calendardata$dateStart,
+                      end = calendardata$dateEnd,
+                      color = calendardata$color)
+    fullcalendar::fullcalendar(caldata, height = "750px")
+  })
+  
+  })
    
-   
-   
-   
+  
+  
+  uploadedImage <- reactive({
+    inFile <- input$upload
+    if(!is.null(inFile)){
+      req(input$upload)
+      myImg = normalizePath(inFile$datapath)
+      return(myImg)
+    }
+  })
+  
+  
+  
+  
+  
+  output$report <- downloadHandler(
+    filename = function() {
+      paste("myReport", sep = '.', switch(
+        input$format, PDF = 'pdf', HTML = 'html', Word = 'docx'
+      ))
+    },
+    
+    content = function(file) {
+      shiny::withProgress( message = "Downloading VAB Package..", value = 0,
+                           {shiny::incProgress(1/10)
+                             shiny::incProgress(2/10)
+                             shiny::incProgress(4/10)
+                             shiny::incProgress(7/10)
+                             shiny::incProgress(8/10)
+                             shiny::incProgress(1)
+                             
+                             src <- normalizePath('projMan/Report.Rmd')
+                             
+                             owd <- setwd(tempdir())
+                             on.exit(setwd(owd))
+                             file.copy(src, 'Report.Rmd', overwrite = TRUE) 
+                             file.copy(uploadedImage(), 'userImage1.png', overwrite = TRUE)
+                             
+                             params <- list(
+                               test = T
+                             )
+                             
+                             out = rmarkdown::render('Report.Rmd', switch(
+                               input$format,
+                               PDF = pdf_document(), HTML = html_document(), Word = word_document()
+                             ), params = params , envir = new.env(parent = globalenv()))
+                             
+                             file.rename(out, file)
+  
+      }
+      )
+      }
+    )
+  
+  
+  
+  
+  
 }
 
 # Run the application 
